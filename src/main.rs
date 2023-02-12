87d292ff42b4bc7370ed4ec05a208054cf8e7dea
Simon Willshire <me@simonwillshire.com>
2023-02-12 22:10:31 +0100

    WIP: Continue playing with tree-sitter, get basic queries working

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,10 @@ extern crate cranelift;
 extern crate cranelift_module;
 extern crate cranelift_jit;
 
+use std::io::Read;
 use std::mem;
+use std::env;
+use std::collections::HashSet;
 
 mod ts;
 use ts::*;
@@ -16,22 +19,53 @@ unsafe fn run_ptr<I, O>(ptr: *const u8, input: I) -> O {
 }
 
 fn main() {
-    let mut frontend = Frontend::from_language(tree_sitter_commonlisp::language()).unwrap();
-    frontend.parse("()");
-
-    /*
     let mut jit = JIT::default();
+    let mut frontend = Frontend::from_language(tree_sitter_commonlisp::language()).unwrap();
 
-    jit.create_data("test", "Testing 123".as_bytes().to_vec()).unwrap();
+    let mut stdin = std::io::stdin();
+    let mut buffer = String::new();
 
-    match jit.compile() {
-        Ok(main_fptr) => {
-            let ret: isize = unsafe {
-                run_ptr(main_fptr, 42)
-            };
-            
-            println!("Executing... {} -> {:?}", 0, ret);
-        },
-        Err(err) => panic!("{}", err),
-    }*/
+    loop {
+        let mut chunk: [u8; 1024] = [0; 1024];
+
+        match stdin.read(&mut chunk[..]) {
+            Ok(len) => {
+                let utf8_chunk = String::from_utf8(chunk[0..len].to_vec()).unwrap();
+                buffer.push_str(&utf8_chunk);
+                frontend.parse(&buffer);
+
+                let matches = frontend.query("
+(list_lit . (sym_lit) @function.builtin (#cl-standard-function? @function.builtin))
+(list_lit . (sym_lit) @function.macro (#cl-standard-macro? @function.macro))
+").unwrap();
+
+                let names = matches.capture_names();
+                for i in 0..matches.pattern_count() {
+                    println!("name: {}", &names[i]);
+                    let m = matches.capture_index_for_name(&names[i]).unwrap() as usize;
+                    for qp in matches.general_predicates(m).iter() {
+                        println!("{:?}", qp);
+                    }
+                    println!("start: {}", matches.start_byte_for_pattern(m));
+                }
+
+                /*
+                match jit.compile() {
+                    Ok(main_fptr) => {
+                        let ret: isize = unsafe {
+                            run_ptr(main_fptr, 42)
+                        };
+                        
+                        println!("Executing... {} -> {:?}", 0, ret);
+                    },
+                    Err(err) => panic!("{}", err),
+                }*/
+
+            },
+            Err(error) => panic!("{:?}", error),
+        }
+        frontend.reset();
+        buffer.clear();
+    }
+
 }
--- src/ts.rs
+++ src/ts.rs
@@ -1,21 +1,27 @@
 extern crate tree_sitter;
 extern crate tree_sitter_commonlisp;
 
-use std::{rc::{
-    Weak,
-    Rc,
-}, any::Any};
+use std::{
+    rc::Rc, 
+    collections::{
+        HashMap,
+    },
+};
 
 use tree_sitter::{
     Parser, 
     Language,
     Tree,
+    Query,
+    QueryError,
+    Node,
 };
 
 pub struct Frontend {
     parser: Parser,
     language: Language,
     tree: Option<Rc<Tree>>,
+    buffer: String,
 }
 
 impl Frontend {
@@ -28,10 +34,12 @@ impl Frontend {
             parser,
             language,
             tree: None,
+            buffer: String::with_capacity(0),
         })
     }
 
     pub fn parse(&mut self, code: &str) {
+        self.buffer = String::from(code);
         let new_tree = if let Some(old_tree) = &self.tree {
             self.parser.parse(code, Some(old_tree.as_ref()))
         } else {
@@ -46,8 +54,51 @@ impl Frontend {
         self.walk_parsed().iter().for_each(|sexp| println!("{}", sexp));
     }
     
-    pub fn walk_parsed(&self) -> Vec<String> {
+    pub fn query(&self, query: &str) -> Result<Query, QueryError> {
+        tree_sitter::Query::new(self.language, query)
+    }
 
+    pub fn query_pattern(&self, pattern: &str, names: &[&str]) -> Result<HashMap<String, Vec<usize>>, QueryError> {
+        let names_str: Vec<String> = names.iter().map(|n| format!("({})", n)).collect();
+        let q_str = format!("[{}] @{}", names_str.join("\n"), pattern);
+
+        let mut ret_map: HashMap<String, Vec<usize>> = HashMap::new();
+
+        let patterns = self.query(&q_str)?;
+        let cap_names = patterns.capture_names();
+        for i in 0..cap_names.len() {
+            let pat_idx = patterns.capture_index_for_name(&cap_names[i]);
+            if pat_idx.is_none() {
+                continue;
+            }
+            let pat_idx = pat_idx.unwrap() as usize;
+
+            let start_byte = patterns.start_byte_for_pattern(pat_idx);
+            let settings = patterns.property_settings(pat_idx);
+            println!("settings for {}: {:?}", &cap_names[i], settings);
+
+            if ret_map.contains_key(&cap_names[i]) {
+                ret_map.get_mut(&cap_names[i]).unwrap().push(start_byte);
+            } else {
+                ret_map.insert(cap_names[i].clone(), vec![start_byte]);
+            }
+        }
+
+        Ok(ret_map)
+    }
+
+    pub fn reset(&mut self) {
+        self.tree = None;
+        self.parser.reset();
+        self.buffer = String::with_capacity(0);
+    }
+
+    fn print_node(&self, n: &Node) -> String {
+        let node_text = &self.buffer[n.start_byte()..n.end_byte()];
+        format!("{}, [{}, {}] = {}, \"{}\"", n.kind(), n.start_position(), n.end_position(), n.to_sexp(), node_text)
+    }
+
+    fn walk_parsed(&self) -> Vec<String> {
         if let Some(tree) = &self.tree {
             let mut cursor = tree.walk();
             let mut nodes = vec![];
@@ -61,24 +112,21 @@ impl Frontend {
                 let node = cursor.node();
 
                 if cursor.goto_next_sibling() {
-                    println!("sibling");
                     nodes.push(node);
                     continue;
                 }
 
                 loop {
-                    println!("inner loop");
                     nodes.push(cursor.node());
 
                     if !cursor.goto_parent() {
                         // back at root, done
-                        return nodes.iter().map(|n| format!("{}, {}, {}", n.id(), n.kind(), n.to_sexp())).collect();
+                        return nodes.iter().map(|n| self.print_node(n)).collect();
                     }
 
                     let node = cursor.node();
                     
                     if cursor.goto_next_sibling() {
-                        println!("inner sibling");
                         nodes.push(node);
                         break;
                     }
@@ -89,4 +137,5 @@ impl Frontend {
             return vec![]
         }
     }
+
 }
